ac1b4614ef50a53141d317cd09c431f4a09a8008
Josh <jschoenberg@princeton.edu>
2022-11-06 18:25:04 -0500

    first attemt at adding invariants20

--- 2DT/checkerDT.c
+++ 2DT/checkerDT.c
@@ -48,12 +48,12 @@ boolean CheckerDT_Node_isValid(Node_T oNNode) {
    }
 
    /* The node's parent should have this child as a child */
-   if (oNParent != NULL) {
+   /* if (oNParent != NULL) {
       if (!Node_hasChild(oNParent, Node_getPath(oNNode), &childID)) {
          fprintf(stderr, "The node's parent does not have it as a child\n");
          return FALSE;
       }
-   }
+   } */
 
    /* The root node should not have a parent */
    if (ulDepth == 1 && oNParent != NULL) {
