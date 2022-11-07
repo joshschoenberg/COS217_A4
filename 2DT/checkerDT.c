ba1722c0331ac501b298a6ebc51e45bcf3aaf31a
Josh <jschoenberg@princeton.edu>
2022-11-07 14:20:08 -0500

    first attemt at adding invariants40

--- 2DT/checkerDT.c
+++ 2DT/checkerDT.c
@@ -87,8 +87,8 @@ boolean CheckerDT_Node_isValid(Node_T oNNode) {
       numberOfEquivalences = 0;
       while (i < Node_getNumChildren(oNParent)) {
          Node_getChild(oNParent, i, &oNSibling);
-         /* fprintf(stdout, "Sibling: %s, com with: %s", Path_getPathname(
-            Node_getPath(oNSibling)), Path_getPathname(Node_getPath(oNNode))); */
+         fprintf(stdout, "Sibling: %s, com with: %s", Path_getPathname(
+            Node_getPath(oNSibling)), Path_getPathname(Node_getPath(oNNode))); 
          if (oNSibling != NULL) {
             int siblingComparison;
             siblingComparison = Node_compare(oNNode, oNSibling);
